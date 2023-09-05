93e513ceeee096dc02261c26b4c07460d9f442ba
Romeo P <romeperl@umd.edu>
2023-09-05 09:10:05 -0400

    still working on bootup scripting

--- flight_preparer.sh
+++ flight_preparer.sh
@@ -19,6 +19,36 @@ White='\033[0;37m'
 color_off='\033[0m'
 
 
-echo -e "\n\tPreparing workspace for flight!"
+echo -e "\n\tPreparing workspace for flight! Make sure this tool is run from the bobot workspace"
 echo -e "\tJJ if you are reading this Peruvian food is better than Pho stay MAD\n"
+
+# Empty the timer data file
 echo -e "${Red}\tErasing timer data from:${color_off} src/bobot_bin/timer.txt"
+> src/bobot_bin/timer.txt
+echo -e "${Green}\tFile contents erased from:${color_off} src/bobot_bin/timer.txt\n"
+
+echo -e "${Red}\tErasing timer data from:${color_off} src/bobot_bin/abs_position.txt"
+> src/bobot_bin/abs_position.txt
+echo -e "${Green}\tFile contents erased from:${color_off} src/bobot_bin/abs_position.txt\n"
+
+echo -e "\tClear Flight Log and Error Log data?\n"
+read clear_log_request
+if [ ${clear_log_request} == "Yes" ] || [ ${clear_log_request} == "yes" ] || [ ${clear_log_request} == "YES" ] || [ ${clear_log_request} == "y" ]
+    then
+    rm src/bobot_bin/log_files/error_logs/*
+    rm src/bobot_bin/log_files/flight_logs/*
+    echo -e "${Green}\tLog data cleared!${color_off}\n"
+fi
+
+echo -e "\tSetting up program to run on boot-up"
+startup_script="/tools/bobot_bootup.sh"
+path=$(realpath "$0")
+DIR=$(dirname "$path")
+FULLPATH=${DIR}${startup_script}
+
+echo -e "[Desktop Entry]\nType=Application\nExec=${FULLPATH}\nHidden=false\nNoDisplay=true\nX-GNOME-Autostart-enabled=true\nName[en_US]=bobot_bootup\nName=bobot_bootup\nComment[en_US]=kys\nComment=SSL ROCKS" > ~/.config/autostart/bobot_bootup.desktop
+
+
+
+
+## TODO: ^ add erasing ability, and add prompt to clear log history
---
+++ tools/bobot_bootup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# |***************************************************************|
+# |* (c) Copyright 2023                                           |
+# |* Balloon Payload Program, University of Maryland              |
+# |* College Park, MD 20742                                       |
+# |* https://bpp.umd.edu                                          |
+# |***************************************************************|
+
+# Script to run on startup - should source the workspace, (possible) build the repository, and the launch the main code
+
+balls=$(dirname)
+
+echo ${balls}
+cd ${PATH}
