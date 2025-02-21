406474fcb3717cc06bf51885645d44ba4830e83d
CJGithub5794 <116574566+CJGithub5794@users.noreply.github.com>
2025-02-21 15:47:08 -0500

    Create bobot_shape_detection.cpp
    beginning of shape detection yarg

---
+++ src/bobot_camera/src/bobot_shape_detection.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+ int main( int argc, char** argv )
+ {
+    VideoCapture cap(0); //capture the video from web cam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+ int iLowH = 0;
+ int iHighH = 179;
+
+ int iLowS = 0; 
+ int iHighS = 255;
+
+ int iLowV = 0;
+ int iHighV = 255;
